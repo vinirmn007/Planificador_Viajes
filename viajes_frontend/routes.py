82d4b62a03e4fd9ef6e8d2cac4a5cc10d0988591
Alexis Roman <alexis.roman@unl.edu.ec>
2025-01-31 00:03:34 -0500

    detalles finales

--- viajes_frontend/routes.py
+++ viajes_frontend/routes.py
@@ -17,38 +17,46 @@ def findRoute(algoritmo, origen, destino):
 
     r = requests.get(url)
     data = r.json().get('data')
-    
+
     return render_template('findRoute.html')
 
 @routes.route('/city/save', methods=['POST'])
 def saveCity():
     headers = {'Content-Type': 'application/json'}
     form = request.form
-    dataForm = {"motivo": form["motivo"],
-                "observaciones": form["observaciones"],
-                "motivo": form["motivo"],
-                "turnoId": int(form["turnoId"])}
+    dataForm = {"nombre": form["name"],
+                "pais": form["country"],
+                "region": form["region"],
+                "clima": form["climate"],
+                "desc": form["description"],
+                "longitud": float(form["lat"]),
+                "latitud": float(form["long"])}
     r = requests.post(URL + 'ciudades/save', data=json.dumps(dataForm), headers=headers)
     data = r.json()
 
     if r.status_code == 200:
         flash('Se ha guardado correctamente', category='info')
+        return redirect('/')
     else:
         flash('No se ha podido guardar', category='error')
-    return redirect('/city/add') 
+    return redirect('/') 
 
 @routes.route('/road/save', methods=['POST'])
 def saveRoad():
     headers = {'Content-Type': 'application/json'}
     form = request.form
-    dataForm = {"ciudadOrigen": form["ciudadOrigen"],
-                "ciudadDestino": form["ciudadDestino"],
-                "distancia": form["distancia"]}
-    r = requests.post(URL + 'rutas/save', data=json.dumps(dataForm), headers=headers)
+    dataForm = {"origen": int(form["origin"]),
+                "destino": int(form["destination"]),
+                "nombre": form["name"],
+                "distancia": float(form["distance"]),
+                "tipo": form["road_type"],
+                "tiempo": float(form["time"])}
+    r = requests.post(URL + 'carreteras/save', data=json.dumps(dataForm), headers=headers)
     data = r.json()
 
     if r.status_code == 200:
         flash('Se ha guardado correctamente', category='info')
+        return redirect('/')
     else:
         flash('No se ha podido guardar', category='error')
-    return redirect('/road/add')   
+    return redirect('/')   
